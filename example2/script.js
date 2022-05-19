bc7af2e8345c7598ee90df4179831afff531f428
Katie Marcks <katiemarcks7@gmail.com>
2022-05-19 08:31:09 -0600

    Log event.target when clicked

--- example2/script.js
+++ example2/script.js
@@ -12,14 +12,14 @@ var grandParent = document.querySelector(".grandparent");
 var parent = document.querySelector(".parent");
 var button = document.querySelector("#click-me");
 
-grandParent.addEventListener("click", function () {
-  console.log("Grandparent");
+grandParent.addEventListener("click", function (event) {
+  console.log(event.target);
 });
 
-parent.addEventListener("click", function () {
-  console.log("Parent");
+parent.addEventListener("click", function (event) {
+  console.log(event.target);
 });
 
-button.addEventListener("click", function () {
-  console.log("Button");
+button.addEventListener("click", function (event) {
+  console.log(event.target);
 });
