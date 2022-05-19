1595c80e9cc0b8ec8f0c397b4a69e3c3eb932ceb
Katie Marcks <katiemarcks7@gmail.com>
2022-05-19 07:57:15 -0600

    Add click event listener to button that logs element clicked

--- example2/script.js
+++ example2/script.js
@@ -1,3 +1,9 @@
-var grandParent = document.querySelector('.grandparent');
-var parent = document.querySelector('.parent');
-var button = document.querySelector('#click-me');
+var grandParent = document.querySelector(".grandparent");
+var parent = document.querySelector(".parent");
+var button = document.querySelector("#click-me");
+
+button.addEventListener("click", logEvent);
+
+function logEvent() {
+  console.log(this);
+}
