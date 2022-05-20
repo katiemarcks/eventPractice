8ab4b37378ea676684faf260178fb99315ae586d
Katie Marcks <katiemarcks7@gmail.com>
2022-05-20 07:01:04 -0600

    Add evt listener to parent element to add listener to  dynamic btns

--- example2/script.js
+++ example2/script.js
@@ -1,13 +1,3 @@
-// var grandParent = document.querySelector(".grandparent");
-// var parent = document.querySelector(".parent");
-// var button = document.querySelector("#click-me");
-
-// button.addEventListener("click", logEvent);
-
-// function logEvent() {
-//   console.log(this);
-// }
-
 var grandParent = document.querySelector(".grandparent");
 var parent = document.querySelector(".parent");
 var button = document.querySelector("#click-me");
--- example3/script.js
+++ example3/script.js
@@ -1,14 +1,22 @@
-var addNewButton = document.querySelector('.add-new');
+var addNewButton = document.querySelector(".add-new");
 
-addNewButton.addEventListener('click', createButton);
+addNewButton.addEventListener("click", createButton);
 
 function showAlert() {
-  alert('You clicked me!');
+  alert("You clicked me!");
 }
 
 function createButton() {
-  var newButton = document.createElement('button');
-  newButton.className = 'click-me';
+  var newButton = document.createElement("button");
+  newButton.className = "click-me";
   newButton.innerText = "New click me button!";
   parent.appendChild(newButton);
 }
+
+var parent = document.querySelector(".parent");
+
+parent.addEventListener("click", function (event) {
+  if (event.target.className === "click-me") {
+    showAlert();
+  }
+});
